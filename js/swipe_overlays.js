059b7fb15e17c6ef83e38e61358da8b79e1a1768
Dion Lao <dion_lao@hotmail.com>
2017-07-05 13:10:42 +1000

    Basic time stamp working

--- js/swipe_overlays.js
+++ js/swipe_overlays.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-  console.log("sdf");
+  updateTime();
 })
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+var dayNames = ["Sun","Mon", "Tues", "Wed", "Thurs","Fri","Sat"];
+
+function updateTime(){
+  today = new Date();
+  day = dayNames[today.getDay()];
+  d = today.getDate();
+  m = monthNames[today.getMonth()];
+  y = today.getFullYear();
+  hour = today.getHours();
+  min = today.getMinutes();
+  sec = today.getSeconds();
+  time = convert2Digit(hour)+":"+convert2Digit(min);
+  dateString = convert2Digit(d)+" / "+m+" "+y+" "+day;
+  $("#date").text(dateString);
+  $("#time").text(time)
+}
+
+function convert2Digit(num){
+  if (num.toString().length == 1){
+    return '0'+num;
+  } else {
+    return num;
+  }
+}
