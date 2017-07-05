4f8ae9021f130635b41e05a36c279f2091cb705c
Daniel Nunns <danielbnunns@gmail.com>
2017-07-05 12:21:50 +1000

    Trying to squash bugs

--- js/webcam_mirror.js
+++ js/webcam_mirror.js
@@ -1,57 +1,20 @@
-$(document).ready(function(){
-  var videoWidth = 500;
-  var videoHeight = 800;
+$(document).ready(function() {
+
   var video = document.querySelector("#videoElement");
 
-  function activateWebcam(width, height) {
-      var compatibleBrowser = hasUserMedia();
-      var videoResolution = getBrowserVideoSettings(compatibleBrowser, width, height);
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-      if (compatibleBrowser) {
-          window.URL = window.URL || window.webkitURL;
-
-          navigator.getUserMedia_ = (   navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-
-        if ( !! navigator.getUserMedia_) {
-            navigator.getUserMedia_({
-              audio: false,
-              video: videoResolution
-          },
-          function(stream) {
-              video.src = window.URL.createObjectURL(stream);
-          },
-          function(err) {
-              console.log("The following error occured: " + err.name);
-          });
-        }
-      } else {
-          console.log("getUserMedia not supported");
-      }
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({
+      video: true
+    }, handleVideo, videoError);
   }
 
-  function hasUserMedia() {
-      navigator.getUserMedia = navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia;
-
-      return navigator.getUserMedia;
+  function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
   }
 
- function getBrowserVideoSettings(browser, width, height) {
-     if (browser === navigator.mozGetUserMedia) {
-         return {
-             width: width,
-             height: height
-         };
-     } else {
-         return {
-             optional: {
-                 minWidth: width,
-                 minHeight: height
-             }
-         };
-     }
+  function videoError(e) {
+    // do something
   }
-
-  activateWebcam(videoWidth, videoHeight);
 });
