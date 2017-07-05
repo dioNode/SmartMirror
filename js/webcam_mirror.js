f9ff63bfa9ea489f8dfdbaeebfee4954d2ae9479
Daniel Nunns <danielbnunns@gmail.com>
2017-07-05 12:09:12 +1000

    Trying to squash bugs

--- js/webcam_mirror.js
+++ js/webcam_mirror.js
@@ -1,15 +1,53 @@
-var video = document.querySelector("#videoElement");
+$(document).ready(function(){
+  var videoWidth = 500;
+  var videoHeight = 800;
+  var video = document.querySelector("#videoElement");
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+  function activateWebcam(width, height) {
+      var compatibleBrowser = hasUserMedia();
+      var videoResolution = getBrowserVideoSettings(compatibleBrowser, width, height);
 
-if (navigator.getUserMedia) {
-    navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
+      if (compatibleBrowser) {
+          window.URL = window.URL || window.webkitURL;
 
-function handleVideo(stream) {
-    video.src = window.URL.createObjectURL(stream);
-}
+          navigator.getUserMedia({
+              audio: false,
+              video: videoResolution
+          },
+          function(stream) {
+              video.src = window.URL.createObjectURL(stream);
+          },
+          function(err) {
+              console.log("The following error occured: " + err.name);
+          });
+      } else {
+          console.log("getUserMedia not supported");
+      }
+  }
 
-function videoError(e) {
-    // do something
-}
+  function hasUserMedia() {
+      navigator.getUserMedia = navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+      return navigator.getUserMedia;
+  }
+
+ function getBrowserVideoSettings(browser, width, height) {
+     if (browser === navigator.mozGetUserMedia) {
+         return {
+             width: width,
+             height: height
+         };
+     } else {
+         return {
+             mandatory: {
+                 minWidth: width,
+                 minHeight: height
+             }
+         };
+     }
+  }
+
+  activateWebcam(videoWidth, videoHeight);
+});
