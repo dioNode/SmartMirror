5105393b5c56423447b61f4738f2ce24a77c381a
Dion Lao <dion_lao@hotmail.com>
2017-07-10 11:11:23 +1000

    working on brightness

--- js/brightness.js
+++ js/brightness.js
@@ -1,37 +1,55 @@
 function moveBrightness(dir){
 
+  brightness = $("#overlay-container").css("background-color");
+  console.log(brightness);
 
-  if (dir == -1){
-    movement = "+=20px";
-  } else {
-    movement = "-=20px";
+  numIntervals = 5;
+  maxBrightness = 1;
+  minBrightness = 0;
+  jumpIntervals = (maxBrightness-minBrightness)/numIntervals;
+
+  brightness += dir * jumpIntervals;
+
+  if (brightness > maxBrightness){
+    brightness = maxBrightness;
+  } else if (brightness < minBrightness){
+    brightness = minBrightness;
   }
 
   $("#brightnessBar .slider").animate({
     top : movement
   });
 
-  minPos = $("#brightnessBar").offset().top;
+  /*minPos = $("#brightnessBar").offset().top;
   sliderPos = $("#brightnessBar .slider").offset().top;
   maxBrightness = $("#brightnessBar").offset().top+$("#brightnessBar").height();
   maxBrightness -= $("#brightnessBar .slider").height();
-  percentBrightness = (sliderPos-minPos) / maxBrightness;
-  
+  percentBrightness = (sliderPos-minPos) / maxBrightness;*/
+
 
   console.log(percentBrightness, sliderPos - minPos);
 
-  if (percentBrightness < 0){
+  /*if (percentBrightness < 0){
     console.log("over");
     percentBrightness = 0;
     $("#brightnessBar .slider").animate({
       top: minPos
     });
   } else if (percentBrightness>1) {
+    console.log("under");
     percentBrightness = 1;
     $("#brightnessBar .slider").animate({
       top: maxBrightness
     });
-  }
+  }*/
 
-  $("#overlay-container").css('opacity', 1-percentBrightness);
+  return newBrightness;
+
+  //$("#overlay-container").css('opacity', 1-percentBrightness);
+}
+
+function calculateOpacity(brightness) {
+  maxOpacity = 0;
+  minOpacity = 0.9;
+  return (1-brightness)*(minOpacity-maxOpacity);
 }
