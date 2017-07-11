5affaa8cfc67c386eb4622675ca0cbe45a8fe646
Daniel Nunns <danielbnunns@gmail.com>
2017-07-11 11:14:27 +1000

    Fixed le delay error.

--- js/mirror-ui.js
+++ js/mirror-ui.js
@@ -1,7 +1,5 @@
 function initialiseUI() {
   setOverlayObject($('.overlay'));
 
-  registerObject($('.slider'), 2000);
-
-  $('.slider').on('press', function() {console.log("YAY");});
+  registerObject($('.slider'), 1000);
 }
