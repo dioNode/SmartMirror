b82083fa3f8098274038ffb8478efeee49a7768b
Dion Lao <dion_lao@hotmail.com>
2017-07-11 11:55:22 +1000

    Testing bubbles

--- js/mirror-ui.js
+++ js/mirror-ui.js
@@ -2,6 +2,7 @@ function initialiseUI() {
   setOverlayObject($('.overlay'));
 
   registerObject($('.slider'), 2000);
+  registerObject($('#messageBox'), 2000);
 
   $('.slider').on('press', function() {console.log("YAY");});
 }
--- js/sideBar.js
+++ js/sideBar.js
@@ -3,23 +3,41 @@ $(document).ready(function(){
     width: 10,
     height: 200,
     speed: 5000
-  });
+  }).jqFloat('stop');
+
+  $("#messsageBox").on('press',toggleBubbles);
+
+  var bubbleRadius = $(".blobBtn").height();
+  var enlargedHeight = bubbleRadius + 5;
+
   $( ".blobBtn" ).hover(
     function() {
       textWidth = $(this).children().outerWidth(true)+20;
+      enlargedWidth = bubbleRadius + textWidth;
       $(this).animate({
         opacity: 0.8,
-        width: "+="+textWidth+"px",
-        height: "+=5px",
+        width: enlargedWidth,
+        height: enlargedHeight
       }, 500)
       $(this).children().show();
     }, function() {
       $(this).animate({
         opacity: 0.8,
-        width: "-="+ textWidth +"px",
-        height: "-=5px",
+        width: bubbleRadius,
+        height: bubbleRadius
       }, 200)
       $(this).children().hide();
     }
   );
 });
+
+function toggleBubbles() {
+  console.log("working");
+  if ($("#messageBox").is(":visible")){
+    $(".blobBtn").jqFloat('stop');
+
+  } else {
+    $(".blobBtn").fadeIn();
+    $(".blobBtn").jqFloat('start');
+  }
+}
