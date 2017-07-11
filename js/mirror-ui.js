d68e7759eef7bbe73f04136a4065a62037425572
Daniel Nunns <danielbnunns@gmail.com>
2017-07-11 12:38:06 +1000

    Can change colours

--- js/mirror-ui.js
+++ js/mirror-ui.js
@@ -2,4 +2,10 @@ function initialiseUI() {
   setOverlayObject($('.overlay'));
 
   registerObject($('.slider'), 1000);
+
+  registerObject($('#colorChangeButton'), 1000);
+  registerObject($('.colorRedBtn'), 1000);
+  registerObject($('.colorGreenBtn'), 1000);
+  registerObject($('.colorOrangeBtn'), 1000);
+  registerObject($('.colorBlueBtn'), 1000);
 }
