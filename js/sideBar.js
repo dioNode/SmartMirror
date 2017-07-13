8cc97899c92fcf87c34308f3a095d9dd75129656
Dion Lao <dion_lao@hotmail.com>
2017-07-13 11:50:15 +1000

    Bubbles almost bug free

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,5 +1,6 @@
 var bubbleOn = true;
 var bubbleRadius;
+var bubbleOpenable = false;
 
 $(document).ready(function(){
   $(".blobBtn").jqFloat({
@@ -8,6 +9,7 @@ $(document).ready(function(){
     speed: 4000,
     minHeight: -200
   });
+  setTimeout(function(){ bubbleOpenable = true; }, 3000);
   $("#messageBox").on('click',toggleBubbles);
   $(".blobBtn").on('press', function(){
     console.log('pressing');
@@ -20,59 +22,86 @@ $(document).ready(function(){
 
   bubbleRadius = $(".blobBtn").height();
 
-  // $( ".blobBtn" ).hover(
-  //   function() {
-  //     openBubble(this);
-  //   }, function() {
-  //     closeBubble(this);
-  //   }
-  // );
+  $( ".blobBtn" ).hover(
+    function() {
+      openBubble(this);
+    }, function() {
+      closeBubble(this);
+    }
+  );
 });
 
 function openBubble(bubble){
-  var enlargedHeight = bubbleRadius + 5;
-  //$(bubble).clearQueue();
-  $(bubble).stop();
-  //$(bubble).jqFloat('pause');
+  console.log(bubbleOpenable);
+  if (bubbleOpenable) {
+    var enlargedHeight = bubbleRadius + 5;
+    $(bubble).clearQueue();
+    $(bubble).stop();
+    //$(bubble).jqFloat('pause');
 
-  $(bubble).children("img").hide();
-  $(bubble).children("p").show();
-  textWidth = $(bubble).children("p").outerWidth(true);
-  enlargedHeight = $(bubble).children("p").outerHeight(true);
-  enlargedWidth = bubbleRadius + textWidth;
-  $(bubble).animate({
-    opacity: 0.8,
-    width: enlargedWidth,
-    height: enlargedHeight
-  }, 500);
+    $(bubble).children("img").hide();
+    $(bubble).children("p").show();
+    textWidth = $(bubble).children("p").outerWidth(true);
+    enlargedHeight = $(bubble).children("p").outerHeight(true);
+    enlargedWidth = bubbleRadius + textWidth;
+    $(bubble).animate({
+      opacity: 0.8,
+      width: enlargedWidth,
+      height: enlargedHeight
+    }, 500);
+  }
 }
 
 function closeBubble(bubble){
+  if (bubbleOpenable){
+    $(bubble).children("p").hide();
+    $(bubble).children("img").show();
+    //$(bubble).jqFloat('unpause');
+    $(bubble).animate({
+      opacity: 0.8,
+      width: bubbleRadius,
+      height: bubbleRadius
+    }, {
+      duration: 200,
+      complete: function() {
+        $(bubble).jqFloat('play');
+      }
+    })
+  }
+
+}
+
+function shutBubbles(){
+  bubble = $(".blobBtn");
   $(bubble).children("p").hide();
   $(bubble).children("img").show();
-  //$(bubble).jqFloat('unpause');
-  $(bubble).animate({
-    opacity: 0.8,
-    width: bubbleRadius,
-    height: bubbleRadius
-  }, {
-    duration: 200,
-    complete: function() {
-      $(bubble).jqFloat('play');
-    }
-  })
+  $(bubble).css("opacity","0.8").css("width",bubbleRadius).css("height",bubbleRadius);
+  // $(bubble).animate({
+  //   opacity: 0.8,
+  //   width: bubbleRadius,
+  //   height: bubbleRadius
+  // }, {
+  //   duration: 200,
+  //   complete: function() {
+  //     //$(bubble).jqFloat('play');
+  //   }
+  // })
 }
 
 function toggleBubbles() {
 
+  $("#messageBox").attr("src","img/chat_open.png");
+
   $(".blobBtn").css("position","relative");
   if (bubbleOn){
+    bubbleOpenable = false;
+    //close bubbles
+    shutBubbles();
     $(".blobBtn").jqFloat('stop');
-    console.log('stopping');
   } else {
-    console.log('starting');
-    $("#messageBox").attr("src","img/chat_open.jpg");
     $(".blobBtn").jqFloat('play');
+    $(".blobBtn").jqFloat('start');
+    setTimeout(function(){ bubbleOpenable = true; }, 3000);
   }
   bubbleOn = !bubbleOn;
 }
